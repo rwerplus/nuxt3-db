34cd836125d702b827e879019840b19266e75fb4
lanfengcloud <localfeng@163.com>
2022-04-18 23:42:05 +0800

    feat: add html head config

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <script lang="ts" setup>
-import './assets/tailwind.scss'
+import './assets/tailwind.scss';
+useHead({
+  titleTemplate: 'My App - %s', // or, title => `My App - ${title}`
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  }
+})
 </script>
 
 
